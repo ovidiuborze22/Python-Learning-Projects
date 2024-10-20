466418445d751159293c946d02d195aa948ccd6d
Ovidiu Borze <ovidiuborze22@gmail.com>
2024-10-20 14:53:38 +0100

    storing user input in Lists

--- Todo_App1/main.py
+++ Todo_App1/main.py
@@ -3,5 +3,9 @@
 #======================================
 
 user_prompt = "Enter a todo: "
-text = input(user_prompt)
-print(text)
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3, "todo4"]
+print(todos)
